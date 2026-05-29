1193c71baa99b15ba0628c06157e37546cdb6b53
EvertBunschoten <evertbun@xs4all.nl>
2026-05-29 14:51:34 +0200

    Test case for two-phase LUT

--- TestCases/NICFD/LUT_Twophase/generate_table.py
+++ TestCases/NICFD/LUT_Twophase/generate_table.py
@@ -1,7 +1,8 @@
-from su2dataminer.config import Config_NICFD
-from su2dataminer.manifold import SU2TableGenerator_NICFD
+# Test case for generating thermodynamic tables for two-phase NICFD problems
 
-# Generate properties of MM with REFPROP library.
+from Common.DataDrivenConfig import Config_NICFD
+from su2dataminer.manifold import TableGenerator_NICFD
+
 config = Config_NICFD()
 config.SetFluid("MM")
 config.SetEquationOfState("REFPROP")
@@ -14,33 +15,36 @@ config.EnableSuperCritical(True)
 
 # Include visosity, conductivity, and vapor quality.
 config.IncludeTransportProperties(True)
+
+# Table limits
 config.UseAutoRange(False)
 config.SetDensityBounds(0.1, 500)
 config.SetEnergyBounds(200e3, 360e3)
 config.SetNpDensity(50)
 config.SetNpEnergy(50)
 
-# Initiate table generator with adaptive triangulation.
-tablegen = SU2TableGenerator_NICFD(config)
-tablegen.setDiscretizationMethod("adaptive")
+tgen = TableGenerator_NICFD(config)
 
-# Specify table resolution for coarse and refined sections.
-tablegen.setTargetNumberOfNodes(10000)
+# Specify a target number of nodes in the table
+tgen.setTargetNodeCount(4e4)
 
-# Relative step size for finite-differences.
-tablegen.setFDStepSize(7e-3)
+# or the maximum cell size
+#tgen.setMaximumCellSize(2.0e-2)
 
-tablegen.tableBoundsFromPointCloud("SU2_solution_data.csv")
-# Optionally, specify thermophysical variables to be included in the table. By default, all variables are included.
-# tablegen.SetTableVars(["Density","Energy","s","p","T", "dsdrho_e","dsde_rho", "d2sdrho2","d2sde2","d2sdedrho","VaporQuality","ViscosityDyn","Conductivity"])
+# Custom refinement; reduce cell size by a specified factor where the 
+# thermodynamic quantity is within specified limits
+tgen.addRefinementCriterion("s", lowerbound=725,upperbound=780, coef=0.4)
+tgen.addRefinementCriterion("Density",0, 50, 0.4)
+tgen.addRefinementCriterion("Density",0, 20, 0.2)
 
-# Specify custom refinement regions (low density, around Trova isentrope)
-tablegen.addRefinementCriterion("Density", 0.0, 50.0, 0.2)
-tablegen.addRefinementCriterion("s", 729.13-2, 729.13+10, 0.2)
+# Apply smoothing to tabulated data. High value = more smoothing
+tgen.setSmoothingParameter(0.05)
 
-# Generate table.
-tablegen.generateTable()
+# Terminal message verbosity
+tgen.setVerbosity(1)
 
-# Write SU2 DRG and vtk table.
-tablegen.WriteTableFile("LUT_adaptive.drg")
-tablegen.WriteOutParaview("vtktable_adaptive")
+tgen.generateTable()
+
+# Write a table file in vtk format so it can be loaded in Paraview.
+tgen.writeParaviewTable("vtktable_twophase")
+tgen.writeSU2Table("SU2table_twophase")
